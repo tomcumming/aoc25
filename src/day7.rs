f1da3def21c048546e31b17dc3b63bf5671bcc13
Tom Cumming <mail@tomcumming.co.uk>
2025-12-07 10:22:19 +0000

    Day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::BTreeSet,
+    collections::{BTreeMap, BTreeSet},
     io::{self, Read},
 };
 
@@ -41,6 +41,47 @@ fn day_7_part_1(inpt: &str) -> usize {
     splits
 }
 
+fn day_7_part_2(inpt: &str) -> usize {
+    let (width, start_x) = {
+        let first_line = inpt.lines().next().unwrap();
+        let start_x = first_line
+            .char_indices()
+            .find(|(_, c)| *c == 'S')
+            .map(|(i, _)| i)
+            .unwrap();
+        (first_line.len(), start_x)
+    };
+
+    let beam_counts = inpt.lines().fold(
+        [(start_x, 1)]
+            .into_iter()
+            .collect::<BTreeMap<usize, usize>>(),
+        |beam_counts, line_str| {
+            let line: Vec<char> = line_str.chars().collect();
+            let not_split = beam_counts
+                .iter()
+                .map(|(k, v)| (*k, *v)) // why not cloned?
+                .filter(|(x, _)| line[*x] == '.' || line[*x] == 'S');
+            let split = beam_counts
+                .iter()
+                .map(|(k, v)| (*k, *v)) // why not cloned?
+                .filter(|(x, _)| line[*x] == '^')
+                .flat_map(|(k, v)| [(k as isize - 1, v), (k as isize + 1, v)])
+                .filter(|(k, _)| *k >= 0 && *k < width as isize)
+                .map(|(k, v)| (k as usize, v));
+
+            not_split
+                .chain(split)
+                .fold(BTreeMap::new(), |mut p, (k, v)| {
+                    p.entry(k).and_modify(|c| *c += v).or_insert(v);
+                    p
+                })
+        },
+    );
+
+    beam_counts.into_values().sum()
+}
+
 fn main() {
     let inpt = {
         let mut inpt = String::new();
@@ -48,11 +89,12 @@ fn main() {
         inpt
     };
     println!("Part 1\t{}", day_7_part_1(&inpt));
+    println!("Part 2\t{}", day_7_part_2(&inpt));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::day_7_part_1;
+    use super::{day_7_part_1, day_7_part_2};
 
     const STR_INPT: &str = ".......S.......
 ...............
@@ -74,5 +116,10 @@ mod tests {
     #[test]
     fn example() {
         assert_eq!(day_7_part_1(STR_INPT), 21);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(day_7_part_2(STR_INPT), 40);
     }
 }
