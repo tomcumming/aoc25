27da8d7c514d602ce4c479682332a5c92f577583
Tom Cumming <mail@tomcumming.co.uk>
2025-12-10 21:07:18 +0000

    Day 10 part 2 fail

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::BTreeSet,
+    collections::{BTreeMap, BTreeSet},
     io::{self, Read},
 };
 
@@ -8,7 +8,7 @@ use itertools::Itertools;
 struct Line {
     lights: BTreeSet<usize>,
     buttons: Vec<BTreeSet<usize>>,
-    _joltage: Vec<usize>,
+    joltage: Vec<usize>,
 }
 
 fn parse_input(inpt: &str) -> Vec<Line> {
@@ -40,7 +40,7 @@ fn parse_input(inpt: &str) -> Vec<Line> {
                     .cloned()
                     .map(parse_button)
                     .collect(),
-                _joltage: parse_joltage(ws[ws.len() - 1]),
+                joltage: parse_joltage(ws[ws.len() - 1]),
             }
         })
         .collect()
@@ -55,6 +55,33 @@ fn do_buttons(bs: &[&BTreeSet<usize>]) -> BTreeSet<usize> {
             } else {
                 p.insert(i);
             }
+            p
+        })
+}
+
+fn bags_of(
+    elems: usize,
+    bag_size: usize,
+) -> Box<dyn Iterator<Item = std::collections::BTreeMap<usize, usize>>> {
+    if bag_size == 0 {
+        return Box::new(std::iter::once(BTreeMap::new()));
+    }
+    Box::new(bags_of(elems, bag_size - 1).flat_map(move |bag| {
+        let max_elem = bag.first_key_value().map(|(k, _)| *k).unwrap_or(elems - 1);
+
+        (0..=max_elem).map(move |i| {
+            let mut bag_ = bag.clone();
+            bag_.entry(i).and_modify(|c| *c += 1).or_insert(1);
+            bag_
+        })
+    }))
+}
+
+fn do_joltage(bs: &[BTreeSet<usize>], js: &BTreeMap<usize, usize>) -> BTreeMap<usize, usize> {
+    js.iter()
+        .flat_map(|(k, v)| bs.get(*k).unwrap().iter().map(|i| (*i, *v)))
+        .fold(BTreeMap::new(), |mut p, (k, v)| {
+            p.entry(k).and_modify(|v_| *v_ += v).or_insert(v);
             p
         })
 }
@@ -74,8 +101,28 @@ fn day_10_part_1(inpt: &str) -> usize {
         .sum()
 }
 
-fn _day_10_part_2(_inpt: &str) -> isize {
-    todo!()
+fn day_10_part_2(inpt: &str) -> usize {
+    let lines = parse_input(inpt);
+    lines
+        .into_iter()
+        .map(|line| {
+            let target: BTreeMap<usize, usize> = line
+                .joltage
+                .into_iter()
+                .enumerate()
+                .filter(|(_k, v)| *v > 0)
+                .collect();
+
+            (0..)
+                .find(|n| {
+                    bags_of(line.buttons.len(), *n).any(|presses| {
+                        let total = do_joltage(&line.buttons, &presses);
+                        total == target
+                    })
+                })
+                .unwrap()
+        })
+        .sum()
 }
 
 fn main() {
@@ -85,12 +132,12 @@ fn main() {
         inpt
     };
     println!("Part 1\t{}", day_10_part_1(&inpt));
-    // println!("Part 2\t{}", day_10_part_2(&inpt));
+    println!("Part 2\t{}", day_10_part_2(&inpt));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::day_10_part_1;
+    use super::{day_10_part_1, day_10_part_2};
 
     const STR_INPT: &str = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
@@ -101,8 +148,8 @@ mod tests {
         assert_eq!(day_10_part_1(STR_INPT), 7);
     }
 
-    // #[test]
-    // fn example2() {
-    //     assert_eq!(day_10_part_2(STR_INPT), _);
-    // }
+    #[test]
+    fn example2() {
+        assert_eq!(day_10_part_2(STR_INPT), 33);
+    }
 }
